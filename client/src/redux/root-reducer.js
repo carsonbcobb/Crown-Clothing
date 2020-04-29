0f20b185b047c3455ef3655306dde6e76a44c34a
carsonbcobb <I3carebears>
2020-04-29 16:59:47 -0700

    bug fixes

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
@@ -9,14 +10,14 @@ import shopReducer from "./shop/shop.reducer";
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["cart"]
+  whitelist: ["cart"],
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
-  shop: shopReducer
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-import rootReducer from "./root-reducer";
+import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "./root-reducer";
 import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -19,4 +20,4 @@ sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
-export default { store, persistor };
+export default { store, persistStore };
