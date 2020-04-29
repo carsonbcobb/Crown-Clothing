5fc011a1890bdd045e9be2c96104f4e4b2e6b297
carsonbcobb <I3carebears>
2020-04-29 15:27:56 -0700

    bug fixes

--- client/src/components/cart-icon/cart-icon.component.jsx
+++ client/src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import {
   CartContainer,
