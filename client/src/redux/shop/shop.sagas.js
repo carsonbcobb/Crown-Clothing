10691b74a4830db70aa378e3240f347e58a2532a
carsonbcobb <I3carebears>
2020-04-29 16:20:01 -0700

    bug fixes

--- client/src/redux/shop/shop.sagas.js
+++ client/src/redux/shop/shop.sagas.js
@@ -1,18 +1,18 @@
-import { takeLatest, call, all, put } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
 
 import {
   firestore,
   convertCollectionsSnapshotToMap,
 } from "../../firebase/firebase.utils";
+
 import {
   fetchCollectionsSuccess,
   fetchCollectionsFailure,
 } from "./shop.actions";
+
 import ShopActionTypes from "./shop.types";
 
 export function* fetchCollectionsAsync() {
-  yield console.log("I am hired");
-
   try {
     const collectionRef = firestore.collection("collections");
     const snapshot = yield collectionRef.get();
