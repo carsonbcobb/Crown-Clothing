21309091c9c371fbb90dc27ad094fc819685d1f7
carsonbcobb <I3carebears>
2020-04-29 16:28:05 -0700

    bug fixes

--- client/src/components/collections-overview/collections-overview.component.jsx
+++ client/src/components/collections-overview/collections-overview.component.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
-import PreviewCollection from "../preview-collection/preview-collection.component";
 import { CollectionsOverviewContainer } from "./collections-overview.styles";
 
 const CollectionsOverview = ({ collections }) => (
   <CollectionsOverviewContainer>
     {collections.map(({ id, ...otherCollectionProps }) => (
-      <PreviewCollection key={id} {...otherCollectionProps} />
+      <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
   </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
